8b2735b3a4df2a6af0982005c0c4f1cba35bba8d
Sander <sander-oostenbrink@hotmail.com>
2019-09-18 15:26:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,25 +15,28 @@ restService.use(bodyParser.json());
 
 restService.post("/hoi", function(req, res) {
     let title = req.body.result.parameters.AudioSample.toLowerCase();
-    if (title = "chicken") {
-    request('https://www.food2fork.com/api/search?key=63c748f9988ed0a2866e6b3db825b5ac&q=' + title , function (error, response, body) {
-      let obj = JSON.parse(body);
-      if(obj.count > 1){
-        return "No results for " + search;
-      }
-      else{
-        let rand = obj.recipes[Math.floor(Math.random() * obj.recipes.length)]
-        let responseObj={
-            "fulfillmentText": "  "
-            ,"fulfillmentMessages": [{"text": {"text": [rand.title]}}]
-            ,"source": " "
+    let search = req.body.queryResult.parameters['any'];
+    console.log(search);
+    return search;
+  //   if (title = "chicken") {
+  //   request('https://www.food2fork.com/api/search?key=63c748f9988ed0a2866e6b3db825b5ac&q=' + title , function (error, response, body) {
+  //     let obj = JSON.parse(body);
+  //     if(obj.count > 1){
+  //       return "No results for " + search;
+  //     }
+  //     else{
+  //       let rand = obj.recipes[Math.floor(Math.random() * obj.recipes.length)]
+  //       let responseObj={
+  //           "fulfillmentText": "  "
+  //           ,"fulfillmentMessages": [{"text": {"text": [rand.title]}}]
+  //           ,"source": " "
 
-        }
-        console.log(responseObj);
-        return res.responseObj;
-      }
-    });
-  }
+  //       }
+  //       console.log(responseObj);
+  //       return res.responseObj;
+  //     }
+  //   });
+  // }
 });
 
 function returnRecipe(search){
