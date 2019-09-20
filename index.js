f697951fdf3a43927feda0aa7ad915db368d312b
Sander <sander-oostenbrink@hotmail.com>
2019-09-20 13:06:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require('request');
 const restService = express();
+let search = "";
 
 restService.use(
   bodyParser.urlencoded({
@@ -13,56 +14,31 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/hoi", function(req, res) {
-    let title = req.body.result.parameters.AudioSample.toLowerCase();
-    let search = req.body.queryResult.parameters['any'];
-    console.log(search);
-    return search;
-  //   if (title = "chicken") {
-  //   request('https://www.food2fork.com/api/search?key=63c748f9988ed0a2866e6b3db825b5ac&q=' + title , function (error, response, body) {
-  //     let obj = JSON.parse(body);
-  //     if(obj.count > 1){
-  //       return "No results for " + search;
-  //     }
-  //     else{
-  //       let rand = obj.recipes[Math.floor(Math.random() * obj.recipes.length)]
-  //       let responseObj={
-  //           "fulfillmentText": "  "
-  //           ,"fulfillmentMessages": [{"text": {"text": [rand.title]}}]
-  //           ,"source": " "
-
-  //       }
-  //       console.log(responseObj);
-  //       return res.responseObj;
-  //     }
-  //   });
-  // }
+restService.post("/", function(req, res) {
+    search = req.body.queryResult.parameters['any'];
+    request('https://www.food2fork.com/api/search?key=2951d8b72aa7ba4dcb402ebbe0c04791&q=' + search , function (error, response, body) {
+      let obj = JSON.parse(body);
+      if(obj.count < 1)
+      {
+        return "No results for " + search;
+      }
+      else
+      {
+        let rand = obj.recipes[Math.floor(Math.random() * obj.recipes.length)];
+        let responseObj={
+            "speech": rand.title
+            ,"displayText": rand.title
+            ,"fulfillmentText" : "How about " + rand.title + "?"
+            ,"source": "helpmeeat"
+            }
+        res.send(responseObj);
+      }
+    });
 });
 
-function returnRecipe(search){
-  request('https://www.food2fork.com/api/search?key=63c748f9988ed0a2866e6b3db825b5ac&q=' + search , function (error, response, body) {
-    let obj = JSON.parse(body);
-    if(obj.count > 1){
-      return "No results for " + search;
-    }
-    else{
-      let rand = obj.recipes[Math.floor(Math.random() * obj.recipes.length)]
-      let responseObj={
-          "fulfillmentText": "  "
-          ,"fulfillmentMessages": [{"text": {"text": [rand.title]}}]
-          ,"source": " "
 
-      }
-      return res.responseObj;
-    }
-  });
-}
 
 
 restService.listen(8000, function() {
   console.log("Server up and listening");
-    // let title = req.body.result.parameters.AudioSample.toLowerCase();
-    // var search = "chicken"
-    // var speech = returnRecipe(search);
-    // console.log(speech)
 });
